f08a3ead4dd621034ce79fb01c9e29dd2012fbac
Alex <alexander.tweed@gmail.com>
2022-04-04 19:53:35 -0600

    First Commit

---
+++ iOS-BLE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-BLE
+//
+//  Created by Alex Tweed on 4/4/22.
+//  Copyright © 2022 Alex Tweed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
